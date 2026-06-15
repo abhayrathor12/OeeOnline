aaafcee1ae264e59c9a957fe792c280274c0a5ac
abhayrathor12 <rathorabhay633@gmail.com>
2026-06-15 18:37:30 +0530

    new

--- Industrial OEE Monitoring Dashboard/vite.config.ts
+++ Industrial OEE Monitoring Dashboard/vite.config.ts
@@ -1,27 +1,15 @@
-import { defineConfig } from 'vite'
-import path from 'path'
-import tailwindcss from '@tailwindcss/vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
+// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [
-    // The React and Tailwind plugins are both required for Make, even if
-    // Tailwind is not being actively used – do not remove them
-    react(),
-    tailwindcss(),
-  ],
-  resolve: {
-    alias: {
-      // Alias @ to the src directory
-      '@': path.resolve(__dirname, './src'),
-    },
+  plugins: [react()],
+  optimizeDeps: {
+    exclude: ['lucide-react'],
   },
-
-  // File types to support raw imports. Never add .css, .tsx, or .ts files to this.
-  assetsInclude: ['**/*.svg', '**/*.csv'],
-   server:{
+  server:{
     host:'0.0.0.0',
-    port:5178,
+    port:5173,
 
   },
-})
+});
