690e33676fbc64c96393c89100215bf8a8452ab1
abhayrathor12 <rathorabhay633@gmail.com>
2026-06-16 11:33:24 +0530

    new message

--- project/vite.config.ts
+++ project/vite.config.ts
@@ -1,15 +1,21 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+import { defineConfig } from 'vite'
+import path from 'path'
+import tailwindcss from '@tailwindcss/vite'
+import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-  optimizeDeps: {
-    exclude: ['lucide-react'],
+  plugins: [
+    react(),
+    tailwindcss(),
+  ],
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, './src'),
+    },
   },
-  server:{
-    host:'0.0.0.0',
-    port:5173,
-
+  assetsInclude: ['**/*.svg', '**/*.csv'],
+  server: {
+    host: '0.0.0.0',
+    port: 5178,
   },
-});
+})
