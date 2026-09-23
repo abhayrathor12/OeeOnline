e255731d5a8517381f2b2c4fa29524e96a48e4b9
abhayrathor12 <rathorabhay633@gmail.com>
2026-09-23 18:26:14 +0530

    aaa

--- project/src/app/api.ts
+++ project/src/app/api.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API_BASE_URL =
-   "https://https://oeebackend.pythonanywhere.com/";
+   "https://oeebackend.pythonanywhere.com/";
 
 const api = axios.create({
   baseURL: API_BASE_URL,
